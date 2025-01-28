e4eade97da61939ab4ca01f6cc641c1560613ded
Dennis Jones <dbjlmc@gmail.com>
2025-01-28 12:20:04 -0800

    Added the ThreadSafeStream class at the top of the file, replaced all cout statements with logger.print() calls

--- concurrency/thread-pool/src/thread_pool.cpp
+++ concurrency/thread-pool/src/thread_pool.cpp
@@ -17,6 +17,22 @@
 #include <future>
 #include <type_traits>
 
+// thread-safe stream wrapper
+class ThreadSafeStream {
+private:
+    std::mutex m_mutex;
+    std::ostream& m_stream;
+
+public:
+    explicit ThreadSafeStream(std::ostream& stream) : m_stream(stream) {}
+
+    template<typename... Args>
+    void print(Args&&... args) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        (m_stream << ... << std::forward<Args>(args)) << std::endl;
+    }
+};
+
 class ThreadPool {
 private:
     // vector to store worker threads
@@ -29,6 +45,8 @@ private:
     std::condition_variable m_condition;
     // flag to signal thread pool shutdown
     bool m_stop;
+    // thread-safe logger
+    ThreadSafeStream m_logger;
 
     // prevent default construction and copying of thread pool
     ThreadPool() = delete;
@@ -36,9 +54,13 @@ private:
     ThreadPool(ThreadPool&&) = delete;
     ThreadPool& operator=(const ThreadPool&) = delete;
     ThreadPool& operator=(ThreadPool&&) = delete;
+
 public:
     // constructor creates specified number of worker threads
-    ThreadPool(size_t numThreads) : m_stop(false) {
+    ThreadPool(size_t numThreads)
+        : m_stop(false), m_logger(std::cout) {
+        m_logger.print("Initializing thread pool with ", numThreads, " threads");
+
         for (size_t i = 0; i < numThreads; ++i) {
             // create worker threads using lambda function
             m_workers.emplace_back([this] {
@@ -56,18 +78,21 @@ public:
 
                         // exit if stopped and no tasks remain
                         if (m_stop && m_tasks.empty()) {
+                            m_logger.print("Worker thread ", std::this_thread::get_id(), " shutting down");
                             return;
                         }
 
                         // get next task from queue
                         task = std::move(m_tasks.front());
                         m_tasks.pop();
+                        m_logger.print("Worker thread ", std::this_thread::get_id(), " dequeued a task");
                     }
 
                     // execute the task
                     task();
                 }
             });
+            m_logger.print("Created worker thread ", m_workers.back().get_id());
         }
     }
 
@@ -77,6 +102,7 @@ public:
             // signal threads to stop
             std::unique_lock<std::mutex> lock(m_queueMutex);
             m_stop = true;
+            m_logger.print("Initiating thread pool shutdown");
         }
 
         // wake up all threads
@@ -86,8 +112,10 @@ public:
         for (std::thread& worker : m_workers) {
             if (worker.joinable()) {
                 worker.join();
+                m_logger.print("Worker thread ", worker.get_id(), " joined");
             }
         }
+        m_logger.print("Thread pool shutdown complete");
     }
 
     // enqueue a task and return a future for the result
@@ -115,6 +143,7 @@ public:
 
             // add task to queue
             m_tasks.emplace([task]() { (*task)(); });
+            m_logger.print("Task enqueued");
         }
 
         // notify one waiting thread
@@ -127,11 +156,14 @@ int main() {
     try {
         // thread pool will use maximum number of concurrent threads supported
         unsigned int threadCount = std::thread::hardware_concurrency();
+        ThreadSafeStream logger(std::cout);
+
         if (threadCount == 0) {
-            std::cout << "Failed to detect the number of concurrent threads supported" << std::endl;
-        } else {
-            std::cout << "This machine supports " << threadCount << " concurrent threads" << std::endl;
+            logger.print("Failed to detect the number of concurrent threads supported");
+            return 1;
         }
+
+        logger.print("This machine supports ", threadCount, " concurrent threads");
 
         // vector to store future results
         std::vector<std::future<int>> results;
@@ -141,21 +173,12 @@ int main() {
             // create thread pool with worker threads
             ThreadPool pool(threadCount);
 
-            // create a shared mutex for cout operations
-            std::mutex cout_mutex;
-
             // add tasks to the thread pool
             for (int i = 0; i < threadCount * 2; ++i) {
-
                 // enqueue task that prints thread id and returns square of input
                 results.emplace_back(
-                    pool.enqueue([i, &cout_mutex] {
-                        {
-                            // protect the cout operation inside the task
-                            std::lock_guard<std::mutex> lock(cout_mutex);
-                            std::cout << "Task " << i << " running on thread "
-                                     << std::this_thread::get_id() << std::endl;
-                        }
+                    pool.enqueue([i, &logger] {
+                        logger.print("Task ", i, " running on thread ", std::this_thread::get_id());
 
                         // do some work
                         std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -166,15 +189,13 @@ int main() {
         }
 
         // get and print results
-        std::mutex cout_mutex;
-        for (auto& result : results) {
-            // protect the cout results
-            std::lock_guard<std::mutex> lock(cout_mutex);
-            std::cout << "Result: " << result.get() << std::endl;
+        for (size_t i = 0; i < results.size(); ++i) {
+            logger.print("Result ", i, ": ", results[i].get());
         }
     } catch (const std::exception& e) {
         // handle any exceptions
-        std::cerr << "Error: " << e.what() << std::endl;
+        ThreadSafeStream logger(std::cerr);
+        logger.print("Error: ", e.what());
         return 1;
     }
 
