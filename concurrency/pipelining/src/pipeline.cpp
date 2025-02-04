b1684eea7cd5e12023a136f32d4079be6e3f12f3
Dennis Jones <dbjlmc@gmail.com>
2025-02-03 16:11:08 -0800

    changes for addition of project_utils.hpp for common code and header definitions

--- concurrency/pipelining/src/pipeline.cpp
+++ concurrency/pipelining/src/pipeline.cpp
@@ -1,124 +1,13 @@
 // MIT License
 // Copyright (c) 2025 dbjwhs
 
-#include <iostream>
 #include <thread>
 #include <queue>
-#include <mutex>
 #include <condition_variable>
 #include <atomic>
 #include <vector>
 #include <chrono>
-#include <fstream>
-
-#define DECLARE_NON_COPYABLE(ClassType) \
-ClassType(const ClassType&) = delete; \
-ClassType& operator=(const ClassType&) = delete
-
-#define DECLARE_NON_MOVEABLE(ClassType) \
-ClassType(ClassType&) = delete; \
-ClassType& operator=(ClassType&) = delete
-
-enum class LogLevel {
-    INFO,
-    NORMAL,
-    CRITICAL
-};
-
-class Logger {
-public:
-    // default constructor uses executable path
-    Logger() {
-        m_log_file.open("app.log", std::ios::app);
-        if (!m_log_file.is_open()) {
-            throw std::runtime_error("Failed to open default log file");
-        }
-    }
-
-    // constructor with custom path
-    explicit Logger(const std::string& path) {
-        if (!std::filesystem::exists(std::filesystem::path(path).parent_path())) {
-            throw std::runtime_error("Invalid path provided: " + path);
-        }
-
-        m_log_file.open(path, std::ios::app);
-        if (!m_log_file.is_open()) {
-            throw std::runtime_error("Failed to open log file: " + path);
-        }
-    }
-
-    ~Logger() {
-        if (m_log_file.is_open()) {
-            m_log_file.close();
-        }
-    }
-
-    // variadic template for logging, way better than overriding log() methods to except
-    // all potential arguments. See README.md for more details.
-    template<typename... Args>
-    void log(LogLevel level, const Args&... args) {
-        std::stringstream message;
-        message << get_utc_timestamp()
-                << " [" << log_level_to_string(level)
-                << "] [Thread:"
-                << std::this_thread::get_id() << "] ";
-        (message << ... << args);
-        message << std::endl;
-
-        std::lock_guard<std::mutex> lock(m_mutex);
-
-        // write to file
-        m_log_file << message.str();
-        m_log_file.flush();
-
-        // write to console
-        if (level == LogLevel::CRITICAL) {
-            std::cerr << message.str();
-        } else {
-            std::cout << message.str();
-        }
-    }
-
-private:
-    std::ofstream m_log_file;
-    std::mutex m_mutex;
-
-    // convert log level to string
-    static std::string log_level_to_string(const LogLevel level) {
-        switch (level) {
-            case LogLevel::INFO:
-                return "INFO";
-            case LogLevel::NORMAL:
-                return "NORMAL";
-            case LogLevel::CRITICAL:
-                return "CRITICAL";
-            default:
-                return "UNKNOWN";
-        }
-    }
-
-    // get current utc timestamp
-    static std::string get_utc_timestamp() {
-        const auto now = std::chrono::system_clock::now();
-        const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-        auto time = std::chrono::system_clock::to_time_t(now);
-
-        std::stringstream ss;
-        struct tm tm_buf;
-#ifdef _WIN32
-        gmtime_s(&tm_buf, &time);
-#else
-        gmtime_r(&time, &tm_buf);
-#endif
-
-        ss << std::put_time(&tm_buf, "%Y-%m-%d %H:%M:%S");
-        ss << '.' << std::setfill('0') << std::setw(3) << ms.count() << " UTC";
-        return ss.str();
-    }
-
-    DECLARE_NON_COPYABLE(Logger);
-    DECLARE_NON_MOVEABLE(Logger);
-};
+#include "../../../headers/project_utils.hpp"
 
 // thread-safe queue implementation that handles concurrent access to data
 template<typename Queue>
